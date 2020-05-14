c632b74e09fa2fd6c30de930fb6fa12d38a75e68
Mikhail <pmixmix3@gmail.com>
2020-05-15 01:22:27 +0300

    first commit

---
+++ src/main/java/com/example/demo/model/User.java
@@ -0,0 +1,23 @@
+package com.example.demo.model;
+
+import lombok.Data;
+
+import javax.persistence.*;
+
+@Data
+@Entity
+@Table(name="users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "firstName")
+    private String firstName;
+
+    @Column(name = "lastName")
+    private String lastName;
+
+
+}
